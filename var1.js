bc0cb7395b82a5b720029fa87edf77e9e8af5ae5
Veronika <112684568+VoznesenskayaV@users.noreply.github.com>
2024-11-01 15:08:00 +0300

    Add files via upload

---
+++ var1.js
@@ -0,0 +1,37 @@
+// массив с числами
+let numbers = [3, 5, 7, 1, 2]
+// переменная для хранения суммы - sum
+let sum = 0
+// суммируем с помощью цикла
+for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i]
+}
+
+// вывод
+console.log("Сумма элементов массива:", sum);
+
+// переменная для мин. значения
+let min = numbers[0]; // допустим, минимальный элемент первый
+
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] < min) {
+        min = numbers[i];
+    }
+}
+console.log("Минимальный элемент массива:", min);
+
+// запрос у пользователя числа
+const prompt = require('prompt-sync')();
+let N = parseInt(prompt("Введите число: "));
+// ищем элементы, которые больше этого числа
+let filteredNumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > N) {
+        filteredNumbers.push(numbers[i]);
+    }
+}
+
+console.log("Элементы больше N:", filteredNumbers);
+
+
