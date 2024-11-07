090a57b8669edab6e24c71646338ab2263b859b1
Veronika <112684568+VoznesenskayaV@users.noreply.github.com>
2024-11-07 10:01:51 +0300

    Add files via upload

---
+++ LW_03.js
@@ -0,0 +1,23 @@
+// Задание 1. Напишите функцию, которая выводит "Hello, World!"
+function sayHello() {
+    console.log("Hello, World!");
+  }
+  
+  sayHello(); 
+
+// Задание 2. Напишите функцию, которая принимает два числа и возвращает их сумму
+function addNumbers(a, b) {
+    return a + b;
+  }
+
+const result = addNumbers(5, 3);   
+console.log(result); // Ожидаемый результат: 8
+  
+// Задание 3. Напишите функцию, которая возвращает квадрат числа
+function squareNumber(num) {
+    return num * num;
+  }
+
+  const newResult = squareNumber(4); 
+  console.log(newResult); 
+  
